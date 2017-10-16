96b7d24ca192225415d172f0b01d7c4438a41acd
BeNsAeI <behnam.saeidi@gmail.com>
2017-10-16 15:34:52 -0700

    readme

--- readMe.m
+++ readMe.m
@@ -1,3 +1,13 @@
+Use this code on 2 conditions:
+1- at your own risk
+2- contact me if about what are you using it in
+
+On that note here are some disclamers:
+
+___
+
+Explaination of how it works:
+
 Algorythm goes like this:
 x^2 = (x-1)^2 + x + (x-1)
 
