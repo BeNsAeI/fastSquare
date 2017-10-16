7fec180b37deebadb12ddde72bde3183e7825c73
BeNsAeI <behnam.saeidi@gmail.com>
2017-10-16 15:51:10 -0700

    Update readMe.m

--- readMe.m
+++ readMe.m
@@ -1,6 +1,6 @@
 Use this code on 2 conditions:
 1- at your own risk
-2- contact me if about what are you using it in
+2- contact me and tell me about what are you using it in
 
 On that note here are some disclamers:
 
@@ -24,3 +24,7 @@ sq(x):
 		return sq(x/10) * 100
 	else:
 		return sq(x-1) + x + (x-1)
+
+Behnam Saeedi:
+Saeedib@oregonstate.edu
+http://people.oregonstate.edu/~Saeedib/
