c5f87dd7c4f9683d408190ad6abf4c707516fae7
BeNsAeI <behnam.saeidi@gmail.com>
2017-10-16 15:51:34 -0700

    Update readMe.m

--- readMe.m
+++ readMe.m
@@ -1,6 +1,6 @@
 Use this code on 2 conditions:
 1- at your own risk
-2- contact me and tell me about what are you using it in
+2- contact me and tell me about what are you using it for
 
 On that note here are some disclamers:
 
